742bf75abc1022ba16fa68069b58d8adbb010ec9
Gaston <gaston.bazan@mi.unc.edu.ar>
2024-02-03 16:25:02 -0300

    Se crea nueva version dataset.py donde se leen archivos.pt

--- dataset.py
+++ dataset.py
@@ -1,77 +1,34 @@
-import os
-import cv2
+import torch
 from torch.utils.data import Dataset
 import numpy as np
-from pydicom import dcmread
-import torch
-import csv
+import os
+import cv2
 
-class INBreastDataset(Dataset):
-    def __init__(self, image_dir, mask_dir, metadata_dir,transform=None):
-        self.image_dir = image_dir
-        self.mask_dir = mask_dir
-        self.metadata_dir = metadata_dir
-        self.transform = transform
-        
-        self.images = os.listdir(image_dir)
-        self.masks = os.listdir(mask_dir)
+class INBreastDataset2012(Dataset):
+    def __init__(self, dict_dir, transform=False, target_transform=False):
+        self.dict_dir = dict_dir
+        self.data = os.listdir(self.dict_dir)
+
 
     def __len__(self):
-        return(len(self.images))    
-    
+        return len(self.data)
+
     def __getitem__(self, index):
-        if torch.is_tensor(index):
-            index = index.tolist()
+        dict_path = os.path.join(self.dict_dir, self.data[index])
+        image = torch.load(dict_path)['image']
+        mass_mask = torch.load(dict_path)['mass_mask']
+        pectoral_muscle_mask = torch.load(dict_path)['pectoral_muscle_mask']
 
-        img_path = os.path.join(self.image_dir, self.images[index])
-        mask_path = os.path.join(self.mask_dir, self.masks[index])
-        image = cv2.normalize(dcmread(img_path).pixel_array, None, alpha=0.0, beta=255.0, norm_type=cv2.NORM_MINMAX).astype(np.float32)
-        mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED).astype(np.float32)
-        #mask[mask > 0.0] = 1.0
 
-        filename = self.masks[index].strip('_mask.jpg')
+        if self.transform:
+            image = self.transform(image)
+            pectoral_muscle_mask = self.transform(pectoral_muscle_mask)
 
-        with open(self.metadata_dir, newline='') as csvfile:
-            patient_list = csv.DictReader(csvfile, delimiter=';', quotechar='|')
-            for row in patient_list:
-                if row['File Name'] == filename:
-                    bi_rads = row['Bi-Rads']
-                    acr = row['ACR']
-                    side = row['Laterality']
-                    view = row['View']
-                    id = row['File Name']
-
-                    break
+        if self.target_transform:
+            mass_mask = self.transform(mass_mask)
         
-        bbox = []
-        alto, ancho = mask.shape
-        cant_mass = len(np.unique(mask))-1
+        sample = {'image': image, 'mass_mask':mass_mask, 'pectoral_muscle_mask':pectoral_muscle_mask}
 
-        if cant_mass > 0:
-            for i in range(cant_mass):
-                copy_mask = mask.copy()
-
-                #Genero una mascara por cada masa que hay en la mascara
-                copy_mask[mask!=i+1] = 0
-                
-                #Obtengo las coordenadas que forman el contorno de la masa
-                cnts,_ = cv2.findContours(copy_mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-                #Obtengo el rectangulo que abarca el contorno obtenido
-                x,y,w,h = cv2.boundingRect(cnts[0])
-                
-                #Transformo los valores obtenidos del bbox al formato YOLO
-                x_centro = x+w//2
-                y_centro = y+h//2
-                bbox.append([x_centro/ancho, y_centro/alto, w/ancho, h/alto, 'mass'])
-        else:
-            bbox = bbox
-
-        if self.transform is not None:
-            augmentations = self.transform(image=image, mask=mask)
-            image = augmentations['image']
-            mask = augmentations['mask']
+            
         
-        sample = {'ID':id, 'image': image, 'mask_mass': mask, 'Bi-Rads': bi_rads, 'ACR': acr, 'Side': side, 'View': view, 'bbox':bbox}
-
         return sample
