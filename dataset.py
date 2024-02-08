ccc6db35a836df9522a40bd9770edf912276c120
Gaston <gaston.bazan@mi.unc.edu.ar>
2024-02-08 15:59:24 -0300

    Se cambia lectura de csv con pandas

--- dataset.py
+++ dataset.py
@@ -15,15 +15,13 @@ class INBreastDataset2012(Dataset):
 
     def __getitem__(self, index):
         dict_path = os.path.join(self.dict_dir, self.data[index])
-        image = torch.load(dict_path)['image'].unsqueeze(0)
-        mass_mask = torch.load(dict_path)['mass_mask'].unsqueeze(0)
-        pectoral_muscle_mask = torch.load(dict_path)['pectoral_muscle_mask'].unsqueeze(0)
+        image = torch.load(dict_path)['image']#.unsqueeze(0)
+        mass_mask = torch.load(dict_path)['mass_mask']#.unsqueeze(0)
+        pectoral_muscle_mask = torch.load(dict_path)['pectoral_muscle_mask']#.unsqueeze(0)
 
 
         if self.transform:
-            image = self.transform(image)
-            pectoral_muscle_mask = self.transform(pectoral_muscle_mask)
-            mass_mask = self.transform(mass_mask)
+            image, mass_mask, pectoral_muscle_mask = self.transform(image, mass_mask, pectoral_muscle_mask)
             
         
         return image, mass_mask
